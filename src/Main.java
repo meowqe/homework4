2f004279a5b9731104027ac3571f74dc4325430c
meowqe <vladk7092@gmail.com>
2022-09-29 17:01:28 +0300

    install commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int a = 1;
+        int b = 1;
+        for(int i = 1; i<n; ++i) {
+            int x = a + b;
+            a = b;
+            b = x;
+            System.out.println(x + " ");
+
+
+        }
+
     }
 }
