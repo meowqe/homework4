98a20586214a4a69fcae0851dc6c56c1dada1119
meowqe <vladk7092@gmail.com>
2022-10-05 18:48:30 +0300

    install commit

--- src/Main.java
+++ src/Main.java
@@ -2,24 +2,28 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int[] a = {5, 1, 5, 2, 4, 3};
-        for (int i=1; i<a.length-1; i++)
-            if (a[i] > a[i-1]) {
-                System.out.println(a[i]);
-            }
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int a = 0;
+        int c = 1;
+
+        while (n>0) {
+            int x = n%10;
+            a += x;
+            n /= 10;
+            System.out.print(x+" ");
+
+
+
+
+        }
+
 
 
 
     }
+
 }
-
-
-
-
-
-
-
-
 
 
 
