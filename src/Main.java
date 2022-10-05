61767d9b92ee66fb214959e3e52a5adafacc6635
meowqe <vladk7092@gmail.com>
2022-10-05 18:53:15 +0300

    install commit

--- src/Main.java
+++ src/Main.java
@@ -2,26 +2,15 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int array[] = {8,1,0,5,-3,6,0,9};
-        int b = array[0];
-
-        m:for(int i = 1; i < array.length; ++i) {
-            if(b > 0){
-                if(array[i] < b && array[i] > 0)
-                    b = array[i];
-            }else
-                b = array[i];
-
-            System.out.println(b);
-            break m;
-        }
-
-
+        int[] a = {5, 1, 5, 2, 4, 3};
+        for (int i=1; i<a.length-1; ++i)
+            if (a[i] > a[i-1]) {
+                System.out.println(a[i]);
+            }
 
 
 
     }
-
 }
 
 
