43991435a104bd36f75608a05e3c398809ca9ba2
meowqe <vladk7092@gmail.com>
2022-10-07 20:19:13 +0300

    install commit

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,5 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        int[] a = {5, 1, 5, 2, 4, 3};
-        for (int i=1; i<a.length-1; ++i)
-            if (a[i] > a[i-1]) {
-                System.out.println(a[i]);
-            }
-
-
+        System.out.println("Hello world");
 
     }
-}
-
-
-
-
-
